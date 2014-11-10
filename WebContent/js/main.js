be9a29eef21a9eb9af665e0244bb2177526f9018
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2014-11-10 19:32:57 +0100

    first version

---
+++ WebContent/js/main.js
@@ -0,0 +1,12 @@
+requirejs.config({
+    // baseUrl: ".",
+    paths : {
+        'jquery' : 'lib/jquery/jquery',
+    // ,
+    // 'jquery-mobile': 'lib/jquery/jquery-mobile'
+    }
+});
+
+require([ 'app' ], function (app) {
+
+});
