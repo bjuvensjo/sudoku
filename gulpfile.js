c23a5c5f8edc6ef153bd70e985ee40427b3569b8
Magnus Bjuvensjö <bjuvensjo@gmail.com>
2015-02-08 11:33:11 +0100

    x

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var manifest = require('gulp-manifest');
 var webserver = require('gulp-webserver');
 
 gulp.task('default', function() {
@@ -7,4 +8,17 @@ gulp.task('default', function() {
             livereload: true,
             open: 'index.html'
         }));
+});
+
+gulp.task('manifest', function(){
+    // gulp.src(['build/*'])
+    gulp.src(['app/src/**/*'])
+        .pipe(manifest({
+            hash: true,
+            preferOnline: true,
+            network: ['http://*', 'https://*', '*'],
+            filename: 'app.manifest',
+            exclude: 'app.manifest'
+        }))
+        .pipe(gulp.dest('build'));
 });
