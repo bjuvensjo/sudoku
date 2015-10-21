7652f6c8b23c45bf8c3e451f4e657c3f336f07ac
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2015-10-21 22:37:02 +0200

    refactoring

--- app/js/view/view.js
+++ app/js/view/view.js
@@ -1,33 +1,18 @@
 module.exports = (function () {
     var React = require('react');
     var ReactDOM = require('react-dom');
-
-    var Container;
     var Controls = require('./controls');
-    var Loader;
     var Menu = require('./menu');
     var Sudoku = require('./sudoku');
-
-    var domElement = document.getElementById("content");
     
-    Loader = React.createClass({
-        render: function() {
-            if (this.props.load) {
-                return <div className="loader"></div>;
-            } else {
-                return <div></div>;
-            }
-        }
-    });
-
-    Container = React.createClass({
+    var Container = React.createClass({
         render: function() {
             return (
                     <div className={"container" + (this.props.model.isSolved() ? " container-solved" : "")}>
                     <Menu errors={this.props.model.getErrors()} clock={this.props.clock} newOnClick={this.props.newOnClick} helpOnClick={this.props.helpOnClick}/>
                     <Sudoku sudoku={this.props.model.getSudoku()} notes={this.props.model.getNotes()} errorIndex={this.props.errorIndex} squareOnClick={this.props.squareOnClick}/>
                     <Controls selectedNumber={this.props.selectedNumber} numberOnClick={this.props.numberOnClick} selectedSelector={this.props.selectedSelector} selectorOnClick={this.props.selectorOnClick}/>
-                    <Loader load={this.props.load}/>
+                    <div className={this.props.load ? "loader" : ""}/>
                     </div>
             );
         }
@@ -35,7 +20,7 @@ module.exports = (function () {
 
     return {
         render: function(props) {
-            ReactDOM.render(<Container {...props}/>, domElement);        
+            ReactDOM.render(<Container {...props}/>, document.getElementById("content"));        
         }
     };
 }());
